fa3252641ea520756d81ff0ab4d111351c575963
Etornam Sunu Bright <sunumacbright@gmail.com>
2020-01-13 17:29:33 +0000

    list view project

---
+++ ListView/ListView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListView
+//
+//  Created by Etornam Sunu on 13/01/2020.
+//  Copyright © 2020 Etornam Sunu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
